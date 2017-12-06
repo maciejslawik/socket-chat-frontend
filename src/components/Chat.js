1015da3d033a6e5f67aadc7a644e50a566745e71
Maciej Sławik <maciekslawik@gmail.com>
2017-12-06 18:21:44 +0100

    Sending and receiving messages, receiving messages on start, keeping 100 messages, handling keypress

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -4,11 +4,12 @@
 import React, {Component} from 'react';
 
 class Chat extends Component {
-
   state = {
     messages: [],
     message: '',
     nickname: '',
+    visible: true,
+    maxMessages: 100,
   };
 
   chat = new WebSocket(process.env.REACT_APP_WEBSOCKET_HOST);
@@ -19,14 +20,23 @@ class Chat extends Component {
   }
 
   initChat() {
-    this.chat.onopen = function(e) {
+    this.chat.onmessage = this.receiveMessages.bind(this);
+  }
 
-    }.bind(this);
-    this.chat.onmessage = function(e) {
-      let messages = this.state.messages.slice();
-      messages.push(e.data);
-      this.setState({messages: messages});
-    }.bind(this);
+  receiveMessages(e) {
+    let messages = this.state.messages.slice();
+    let receivedMessages = JSON.parse(e.data);
+    receivedMessages.forEach(function(receivedMessage) {
+      this.pushMessageToMessages(messages, receivedMessage);
+    }.bind(this));
+    this.setState({messages: messages});
+  }
+
+  pushMessageToMessages(messages, newMessage) {
+    messages.push(newMessage);
+    if (messages.length > this.state.maxMessages) {
+      messages.shift();
+    }
   }
 
   sendMessage() {
@@ -37,7 +47,11 @@ class Chat extends Component {
   }
 
   getMessageContent() {
-    return '<strong>' + this.state.nickname + '</strong>: ' + this.state.message;
+    let message = {
+      nickname: this.state.nickname,
+      message: this.state.message,
+    };
+    return JSON.stringify(message);
   }
 
   handleNicknameChange(e) {
@@ -48,36 +62,68 @@ class Chat extends Component {
     this.setState({message: e.target.value});
   }
 
+  clearChat() {
+    this.setState({messages: []});
+  }
+
+  toggleChatVisibility() {
+    this.setState({visible: !this.state.visible});
+  }
+
+  handleKeyPress(e) {
+    if (e.key === 'Enter') {
+      this.sendMessage();
+    }
+  }
+
   render() {
     return (
-        <div className={'chat-window'}>
-          <div className={'bar top-bar'}>
+        <div className={`chat-window ${this.state.visible ? '' : 'folded'}`}>
+          <div onClick={this.toggleChatVisibility.bind(this)}
+               className={'bar top-bar'}>
             <span>CHAT</span>
           </div>
-          <div className={'chat-content'}>
-            <div className={'chat-messages'}>
-              {
-                this.state.messages.map(function(message) {
-                  return <span
-                      dangerouslySetInnerHTML={{__html: message}} />;
-                })
-              }
-            </div>
-            <input className={'input message'}
-                   placeholder={'message'}
-                   value={this.state.message}
-                   onChange={this.handleMessageChange.bind(this)}
-            />
-          </div>
-          <div className={'bar bottom-bar'}>
-            <button onClick={this.sendMessage.bind(this)} type={'button'}
-                    className={'btn btn-success send'}>Send
-            </button>
-            <input className={'input name'}
-                   placeholder={'nickname'}
-                   value={this.state.nickname}
-                   onChange={this.handleNicknameChange.bind(this)}/>
-          </div>
+          {this.state.visible ?
+              <div className={'chat-content'}>
+                <div className={'chat-messages'}>
+                  {
+                    this.state.messages.map(function(message, key) {
+                      return <div key={key}
+                                  dangerouslySetInnerHTML={{
+                                    __html: '<strong>' + message.nickname +
+                                    '</strong>: ' + message.message,
+                                  }}/>;
+                    })
+                  }
+                </div>
+                <input className={'input message'}
+                       placeholder={'message'}
+                       value={this.state.message}
+                       onChange={this.handleMessageChange.bind(this)}
+                       onKeyPress={this.handleKeyPress.bind(this)}
+                />
+              </div>
+              :
+              null
+          }
+          {this.state.visible ?
+              <div className={'bar bottom-bar'}>
+                <button onClick={this.sendMessage.bind(this)} type={'button'}
+                        className={'btn btn-success send'}>Send
+                </button>
+                <input className={'input name'}
+                       placeholder={'nickname'}
+                       value={this.state.nickname}
+                       onChange={this.handleNicknameChange.bind(this)}
+                       onKeyPress={this.handleKeyPress.bind(this)}
+                />
+                <button onClick={this.clearChat.bind(this)} type={'button'}
+                        className={'btn btn-success clear'}>clear
+                </button>
+              </div>
+              :
+              null
+          }
         </div>
     );
   }
